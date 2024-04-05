b5b6da3f0d7b6b796c1df2c9a8b0f93fcdfb20b0
Picadio <sashabedryk2004@gmail.com>
2024-04-05 09:37:56 +0300

    add parse text func

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
+import re
 def load_file(file_path):
     with open(file_path, 'r') as f:
         text = f.read()
     return text
 
 
+def parse_text(text: str) -> (int, int):
+    sentence_end_symbols = "[.!?]"
+    distributors_symbols = "[,.:; !?]"
 
+    text = (text.replace("\n", " ")
+            .replace(", ", ",")
+            .replace(". ", ".")
+            .replace("...", ".")
+            .replace(": ", ":")
+            .replace("; ", ";")
+            .replace("! ", "!")
+            .replace("? ", "?"))
 
+    count_words = len(re.split(distributors_symbols, text))
+    count_sentences = len(re.split(sentence_end_symbols, text))
 
+    return count_words - 1, count_sentences - 1
 
 
 
 
 
 if __name__ == '__main__':
-    pass
+    text = load_file("test.txt")
+    print(parse_text(text))
